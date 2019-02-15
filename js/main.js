876ed830c4b2e23ba5d26811b1f24949b9550a36
Lukas Dias <diaslukas19@gmail.com>
2019-02-15 01:55:06 -0300

    primeiras mudanças

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 // hello
+function iconeMenu(tipo){
+    if (tipo == 1){
+        _left = "952px";
+        _top = "-185px"
+        icone = "src/home-icon.png";
+    }
+    if (tipo == 2){
+        _left = "1090px";
+        _top = "-185px";
+        icone = "src/search-icon.png";
+    }
+    if (tipo == 3){
+        _left = "1257px";
+        _top = "-185px";
+        icone = "src/project-icon.png";
+    }
+    document.getElementById("icone-menu").style.top = _top
+    document.getElementById("icone-menu").style.left = _left;
+    document.getElementById("icone-menu").src = icone;  
+}
