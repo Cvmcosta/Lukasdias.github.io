156027ddc91a06bc73d55938d384d34f01eddd10
Lukas Dias <diaslukas19@gmail.com>
2019-10-05 21:17:43 -0300

    [Rebase]

---
+++ precache-manifest.ccda18d442d7dd26241d625aa3e321d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86486908325aef24fe3b6e160646848d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f0d1b94c1877750302bd",
+    "url": "./static/css/main.32a86ada.chunk.css"
+  },
+  {
+    "revision": "759c866bafa86fd27cc5",
+    "url": "./static/js/2.353733da.chunk.js"
+  },
+  {
+    "revision": "f0d1b94c1877750302bd",
+    "url": "./static/js/main.89b105cd.chunk.js"
+  },
+  {
+    "revision": "49b26d9def15d631ecf4",
+    "url": "./static/js/runtime-main.4e5930c3.js"
+  },
+  {
+    "revision": "3c05cff3df06667c04170555219b6d2f",
+    "url": "./static/media/Background.3c05cff3.png"
+  },
+  {
+    "revision": "a8450bf234a8576cfe35ec2049920434",
+    "url": "./static/media/Projeto-1.a8450bf2.png"
+  }
+]);
